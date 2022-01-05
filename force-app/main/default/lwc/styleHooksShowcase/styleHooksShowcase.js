62d0a18498cdb4906da4631abab5c8779a7f8273
krzysztof-nowak <86788788+krzysztof-nowak@users.noreply.github.com>
2022-01-05 23:59:20 +0100

    Init

---
+++ force-app/main/default/lwc/styleHooksShowcase/styleHooksShowcase.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Krzysztof Nowak - mooza inspire s.r.o..
+ */
+
+import { LightningElement } from 'lwc';
+
+
+export default class StyleHooksShowcase extends LightningElement {
+
+}
